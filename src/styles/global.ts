246c6ba4a5b7ae2fe9e412c05d4b9c65bc64dba0
Flávio Ferreira <business.flavioferreira@gmail.com>
2021-05-17 09:50:15 -0300

    background

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from 'styled-components';
+import githubBackground from '../assets/github-background.svg';
 
 export default createGlobalStyle`
   * {
@@ -9,11 +10,21 @@ export default createGlobalStyle`
     }
 
     body {
-      background: #F0F0F5;
+      background: #F0F0F5 url(${githubBackground}) no-repeat 70% top;
       -webkit-font-smoothing: antialiased;
     }
     body, input, button {
       font: 16px Roboto, sans-serif;
+    }
+
+    #root {
+      max-width: 960px;
+      margin: 0 auto;
+      padding: 40px 20px;
+    }
+
+    button {
+      cursor: pointer;
     }
 
 `;
