45f22d8c9338bbae60937d8e5a03d6637d435995
Flávio Ferreira <business.flavioferreira@gmail.com>
2021-05-17 15:51:58 -0300

    Dashboard structure

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,58 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/37152253?v=4"
+            alt="Flávio Ferreira"
+          />
+          <div>
+            <strong>Rocketseat/unform</strong>
+            <p>Performance-focused API for React forms</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/37152253?v=4"
+            alt="Flávio Ferreira"
+          />
+          <div>
+            <strong>Rocketseat/unform</strong>
+            <p>Performance-focused API for React forms</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/37152253?v=4"
+            alt="Flávio Ferreira"
+          />
+          <div>
+            <strong>Rocketseat/unform</strong>
+            <p>Performance-focused API for React forms</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
